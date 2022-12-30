30e1b51e1e5facfac6f1ebeb5fcb035aa4b8d8f9
Tanmay Kamble <tanmay.kamble@sprinklr.com>
2022-12-30 17:06:09 +0530

    Revert "From learningGit2"
    This reverts commit a6d8115057d4c5b1ee9b3783c379309c44ccc1a9.

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,6 +2,5 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("Devclass");
         System.out.println("Hello World!!");
-        System.out.println("hello form learningGit2");
     }
 }
